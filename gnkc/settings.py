2dc0f196c343280b7be884c4de648200324fa04a
Parmjit Singh <psg@pop-os.localdomain>
2020-10-07 11:10:56 +0100

    teacher and student apps and layout changes

--- gnkc/settings.py
+++ gnkc/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'index.apps.IndexConfig',
+    'students.apps.StudentsConfig',
+    'teachers.apps.TeachersConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
